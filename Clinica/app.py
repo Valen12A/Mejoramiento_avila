6257770400809e9bcd38a0e10cf5539a5545666a
valentina <valeavila717@gmail.com>
2023-07-13 14:17:19 -0500

    3

--- Clinica/app.py
+++ Clinica/app.py
@@ -12,7 +12,7 @@ while True:
     if opcion == 1:
         print ()
         print("__MENU PACIENTE__")
-        print("1. Crear cita")
+        print("1. Registar cita")
         print("2. Consultar cita")
         print("3. Eliminar cita")
         print("4. Seleccionar especialidad")
@@ -20,7 +20,7 @@ while True:
         if opcion==0:
             break
         elif opcion == 1:
-            Paciente.CrearCita(Paciente.citas)
+            Paciente.registrarCita(Paciente.citas)
         elif opcion == 2:
             Paciente.consultarCita()
         elif opcion == 3:
@@ -36,6 +36,6 @@ while True:
         if opcion ==0:
             break
         elif opcion == 1:
-            Medico.buscarCita()
+            Medico.ConsultarCitasAgendadas()
         else:
             print('Ingrese una opcion valida')
